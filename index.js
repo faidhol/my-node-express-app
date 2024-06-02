d4940185345f48a992ec996d168b3d7d2648bf3d
faidholanwar17 <faidholanwar17@gmail.com>
2024-06-02 14:08:37 +0000

    npm mongodb

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path')
-
 const app = express();
-
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+// models
+const Place = require('./models/place');
+
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test')
+  .then((result) => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
